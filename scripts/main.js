982687d5f0a3ca7e7e9f56f9ada54e39d254c500
yul2 <yul2@g-bits.com>
2023-07-26 09:42:35 +0800

    提交web资源

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/task_icon.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+myButton.onclick = () => {
+    setUserName();
+};
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+// localStorage.clear();
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
